8077c3b6052dfc86ddc1f9df87cd4aec44e5fad6
kerimcaglar <kerim.caglarr@gmail.com>
2019-12-01 11:28:25 +0300

    added iOS versions and, alert function

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "TRSaldaViewDEV",
+    platforms: [
+        .iOS(.v8), .iOS(.v9), .iOS(.v10), .iOS(.v11), .iOS(.v12), .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
