1df33fdc4f16bb50a08da98c83ff9bf7d90ab7ce
kerimcaglar <kerim.caglarr@gmail.com>
2019-12-01 14:16:32 +0300

    changed func

--- Sources/TRSaldaViewDEV/TRSaldaViewDEV.swift
+++ Sources/TRSaldaViewDEV/TRSaldaViewDEV.swift
@@ -2,11 +2,8 @@ import UIKit
 
 extension UIViewController {
     public func showAlertAction(title: String, message: String){
-        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
-        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: {(action:UIAlertAction!) in
-            print("Action")
-        }))
-        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.default, handler: nil))
-        self.present(alert, animated:true, completion: nil)
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Tamam", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
 }
