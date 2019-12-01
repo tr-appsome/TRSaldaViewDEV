70a19059f93c11d0fa947c8c881334069ffcd742
kerimcaglar <kerim.caglarr@gmail.com>
2019-12-01 21:11:24 +0300

    changed func name

--- Sources/TRSaldaViewDEV/TRSaldaViewDEV.swift
+++ Sources/TRSaldaViewDEV/TRSaldaViewDEV.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 extension UIViewController {
-    public func showAlertAction(title: String, message: String){
+    public func showAlert(title: String, message: String){
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Tamam", style: .default, handler: nil))
         self.present(alert, animated: true, completion: nil)
