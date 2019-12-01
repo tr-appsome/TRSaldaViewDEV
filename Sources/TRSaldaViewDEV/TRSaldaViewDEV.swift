e19f357e96785edba90de992809ebffd15ff627c
kerimcaglar <kerim.caglarr@gmail.com>
2019-12-01 14:07:17 +0300

    changed class with extension

--- Sources/TRSaldaViewDEV/TRSaldaViewDEV.swift
+++ Sources/TRSaldaViewDEV/TRSaldaViewDEV.swift
@@ -1,7 +1,6 @@
-import Foundation
 import UIKit
 
-public class Alert:UIViewController {
+extension UIViewController {
     public func showAlertAction(title: String, message: String){
         let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
         alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: {(action:UIAlertAction!) in
