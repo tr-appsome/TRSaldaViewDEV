06854127d911f3a4cb675a7f12c6d03b72e2254a
kerimcaglar <kerim.caglarr@gmail.com>
2019-12-01 11:39:43 +0300

    added public key

--- Sources/TRSaldaViewDEV/TRSaldaViewDEV.swift
+++ Sources/TRSaldaViewDEV/TRSaldaViewDEV.swift
@@ -1,8 +1,8 @@
 import Foundation
 import UIKit
 
-class Alert:UIViewController {
-    func showAlertAction(title: String, message: String){
+public class Alert:UIViewController {
+    public func showAlertAction(title: String, message: String){
         let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
         alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: {(action:UIAlertAction!) in
             print("Action")
